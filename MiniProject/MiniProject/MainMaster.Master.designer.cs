6788422d3630ff304d3d4a9db9ea001877bd2601
Nikhil Peter Raj <nikhilpeter93@gmail.com>
2014-11-02 13:04:27 +0530

    Modify Master for dynamic Page Header

--- MiniProject/MiniProject/MainMaster.Master.designer.cs
+++ MiniProject/MiniProject/MainMaster.Master.designer.cs
@@ -22,6 +22,15 @@ namespace MiniProject {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContentPlaceHolder;
         
         /// <summary>
+        /// PageHeaderPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder PageHeaderPlaceHolder;
+        
+        /// <summary>
         /// BodyContentPlaceHolder control.
         /// </summary>
         /// <remarks>
